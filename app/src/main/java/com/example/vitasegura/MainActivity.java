5658752689a4569fcfef7c2c0a9da704c74ac98c
Pelayo04 <a22300954@ceti.mx>
2025-12-19 12:40:43 -0600

    Redireccion a Main del Familiar

--- app/src/main/java/com/example/vitasegura/MainActivity.java
+++ app/src/main/java/com/example/vitasegura/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.vitasegura;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvMonitoreoLink;
     private Button btnRegistrarse, btnIniciarSesion;
 
     @Override
@@ -21,9 +23,18 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        tvMonitoreoLink = findViewById(R.id.tv_monitoreo_link);
         btnRegistrarse = findViewById(R.id.btn_registrarse);
         btnIniciarSesion = findViewById(R.id.btn_iniciar_sesion);
 
+        tvMonitoreoLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirección a la interfaz principal del cuidador/familiar
+                Intent intent = new Intent(MainActivity.this, MainFamiliarActivity.class);
+                startActivity(intent);
+            }
+        });
 
         btnRegistrarse.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
